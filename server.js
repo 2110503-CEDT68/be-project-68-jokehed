26be1d81e36e91f46c9cb778537293fe0a38bd33
69ScoresThatsME <6833180721@student.chula.ac.th>
2026-02-24 13:18:33 +0700

    Assignment07

---
+++ server.js
@@ -0,0 +1,34 @@
+const { setServers } = require("node:dns/promises");
+setServers(["1.1.1.1", "8.8.8.8"]);
+
+const express=require('express');
+const dotenv=require('dotenv');
+const hospitals=require(`./routes/hospitals`);
+const connectDB=require('./config/db');
+const auth=require('./routes/auth');
+const cookieParser=require("cookie-parser");
+const appointments=require('./routes/appointments');
+
+dotenv.config({path:'./config/config.env'});
+
+connectDB();
+
+const app=express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(`/api/v1/hospitals`,hospitals);
+app.use(`/api/v1/auth`,auth);
+
+app.use('/api/v1/appointments',appointments);
+
+app.set('query parser','extended');
+
+const PORT=process.env.PORT||5000;
+const server=app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
